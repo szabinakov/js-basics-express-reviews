090ea4ef7af95a5dd65a97d8b653b11ab19a699b
David Ding <dingtechxing16@gmail.com>
2023-04-30 00:36:35 +0100

    Project setup and passed strings test

---
+++ src/lib/booleans.js
@@ -0,0 +1,83 @@
+function negate(a) {
+  return !a;
+};
+
+function both(a, b) {
+  return a && b;
+}
+
+function either(a, b) {
+ return a || b;
+};
+
+function none(a, b) {
+  return !(a || b);
+};
+
+function one(a, b) {
+  return (a && !b) || (!a && b);
+};
+
+function truthiness(a) {
+  return Boolean(a);
+};
+
+function isEqual(a, b) {
+  return a === b;
+};
+
+function isGreaterThan(a, b) {
+  return a > b
+};
+
+function isLessThanOrEqualTo(a, b) {
+  return a <= b
+};
+
+function isOdd(a) {
+  return a % 2 !== 0;
+};
+
+function isEven(a) {
+  return a % 2 === 0;
+};
+
+function isSquare(a) {
+  return a>= 0 && Math.sqrt(a) % 1 === 0;
+};
+
+function startsWith(char, string) {
+  return string.charAt(0) === char;
+};
+
+function containsVowels(string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i].toLowerCase())) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isLowerCase(string) {
+  return string === string.toLowerCase();
+};
+
+module.exports = {
+  negate,
+  both,
+  either,
+  none,
+  one,
+  truthiness,
+  isEqual,
+  isGreaterThan,
+  isLessThanOrEqualTo,
+  isOdd,
+  isEven,
+  isSquare,
+  startsWith,
+  containsVowels,
+  isLowerCase
+};
