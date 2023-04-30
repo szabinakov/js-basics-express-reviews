24c85d056a6b74d48d5b68e259ec73fef1059c1b
David Ding <dingtechxing16@gmail.com>
2023-04-30 01:00:24 +0100

    Customised output

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings');
 
 const app = express();
 
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200).json({ result: 'Hello, world!' });
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: sayHello(req.params.string) });
 });
 
 module.exports = app;
